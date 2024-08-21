40bd2abc8b70fcecfba17ebedaa6c5edaf48f92e
E-cvzb <cevizbas01@gmail.com>
2024-08-21 23:06:49 +0300

    Add files via upload

---
+++ SQLÖdev6.sql
@@ -0,0 +1,13 @@
+SELECT AVG(rental_rate) FROM film ;
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+SELECT * FROM film 
+WHERE rental_rate =0.99
+ORDER BY length DESC
+LIMIT 1;
+
+SELECT COUNT (DISTINCT replacement_cost) FROM film 
+WHERE length>15 ;
+ 
